8948a9966b3efc86e4c03eb89a3b5247c9e4fa95
Solobored <josneiba10@gmail.com>
2025-03-06 11:11:30 -0300

    commit

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const josueNeiculeo = (req, res) => {
+  res.send('Josue Neiculeo');
+};
+
+const hannahRoute = (req,res) => {
+  res.send('Hannah Birch')
+};
+
+module.exports = {
+  josueNeiculeo,
+  hannahRoute,
+};
